6e09df82c1626d8f0ef0b3510f9cd22225be2ae4
Bigbrass <donrwalsh@gmail.com>
2017-06-06 18:42:11 -0500

    Add ability to skip already logged files
    Done by adding date and time field that are consulted before proceeding
    through parsing and adding a file. Also added quick logic to skip an
    unpanse-able file in the bunch.

--- converter.py
+++ converter.py
@@ -11,18 +11,20 @@ def strip_sig(str):
 def write_unicode(text, charset='utf-8'):
     return text.encode(charset)
 
-list_of_keys = ('SSID' , 'BSSID' , 'capabilities', 'frequency', 'level', 'centerFreq0',
+list_of_keys = ('Date', 'Time', 'SSID' , 'BSSID' , 'capabilities', 'frequency', 'level', 'centerFreq0',
                 'centerFreq1', 'channelWidth', 'operatorFriendlyName',
                 'venueName', 'is80211mcResponder', 'isPasspointNetwork')
 
 for filename in os.listdir('raw_files'):
-    if ".txt" in filename:
-        print("Now working on " + filename)
+    if ".txt" in filename and filename != "Jackson 05-30 15_36.txt":
+        print("Loading " + filename)
         building = filename.split(' ', 1)[0]
         with open('/Users/lfarvour/PycharmProjects/wifi-analyzer/raw_files/' + filename, 'r') as myfile:
             data=myfile.read()
         xmldoc = minidom.parseString(strip_sig(data))
         scan = xmldoc.getElementsByTagName("scanResult")
+        date = filename.split(' ', 3)[1]
+        time = filename.split(' ', 3)[2][:-4].replace("_", ":")
         for key in scan[0].attributes.keys():
             if key not in list_of_keys:
                 raise Exception('The XML source txt file contains incorrect column names. Terminating')
@@ -30,10 +32,24 @@ for filename in os.listdir('raw_files'):
             with open(building + ".csv",'wb') as resultFile:
                 wr = csv.writer(resultFile)
                 wr.writerow(list_of_keys)
+        else:
+            buildingcsv = csv.reader(open(building + '.csv'))
+            existingTimes = []
+            existingDates = []
+            for row in buildingcsv:
+                if row[1] not in existingTimes and row[1] != "Time":
+                    existingTimes.append(row[1])
+                if row[0] not in existingDates and row[0] != "Date":
+                    existingDates.append(row[0])
+            if date in existingDates and time in existingTimes:
+                print(filename + " has already been logged.")
+                continue
+        print("Logging " + filename)
         with open(building + ".csv",'a') as writeTo:
             wt = csv.writer(writeTo)
             for result in scan:
-                addList = []
-                for key in list_of_keys:
+
+                addList = [date, time]
+                for key in list_of_keys[-11:]:
                     addList.append(write_unicode(result.attributes[key].value))
                 wt.writerow(addList)
