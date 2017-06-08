9fc431027b594741494671cd46ef91169cfe794a
Bigbrass <donrwalsh@gmail.com>
2017-06-08 14:26:08 -0500

    Record omission and duplicate removal
    Records with specific SSIDs are removed, BSSID is templated properly
    and after import each file is scanned and duplicates are removed.

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import os
 import csv
 from xml.dom import minidom
+
 #Update the .gs to move things to raw_files
 #Also if there's any work to be done on the timestamp, I'm using raw '_' files.
 
@@ -11,13 +12,36 @@ def strip_sig(str):
 def write_unicode(text, charset='utf-8'):
     return text.encode(charset)
 
+def removeDuplicates(filename):
+    if filename[-4:] != ".csv":
+        raise Exception(filename + ' is not a csv file.')
+    os.rename(filename, '_' + filename)
+    inFile = csv.reader(open("_" + filename))
+    with open(filename, "wb") as outFile:
+        writeTo = csv.writer(outFile)
+        matchPairs = []
+        for row in inFile:
+            if row[2]+row[3] in matchPairs:
+                continue
+            else:
+                writeTo.writerow(row)
+                matchPairs.append(row[2]+row[3])
+    os.remove('_' + filename)
+
 list_of_keys = ('Date', 'Time', 'SSID' , 'BSSID' , 'capabilities', 'frequency', 'level', 'centerFreq0',
                 'centerFreq1', 'channelWidth', 'operatorFriendlyName',
                 'venueName', 'is80211mcResponder', 'isPasspointNetwork')
 
+
+#Remove all output csv files on start:
+filelist = [ f for f in os.listdir(".") if f.endswith(".csv") ]
+for f in filelist:
+    os.remove(f)
+
 for filename in os.listdir('raw_files'):
     if ".txt" in filename and filename != "Jackson 05-30 15_36.txt":
-        print("Loading " + filename)
+        omit = 0
+        duplicate = 0
         building = filename.split(' ', 1)[0]
         with open('/Users/lfarvour/PycharmProjects/wifi-analyzer/raw_files/' + filename, 'r') as myfile:
             data=myfile.read()
@@ -44,11 +68,19 @@ for filename in os.listdir('raw_files'):
             if date in existingDates and time in existingTimes:
                 print(filename + " has already been logged.")
                 continue
-        print("Logging " + filename)
         with open(building + ".csv",'a') as writeTo:
             wt = csv.writer(writeTo)
             for result in scan:
-                addList = [date, time]
-                for key in list_of_keys[-12:]:
-                    addList.append(write_unicode(result.attributes[key].value))
-                wt.writerow(addList)
+                if result.attributes['SSID'].value not in ["UofM Secure", "eduroam", "UofM-Guest"]:
+                    addList = [date, time]
+                    for key in list_of_keys[-12:]:
+                        if key == "BSSID": addList.append(result.attributes[key].value.replace(":", "")[:-2] + "%")
+                        else: addList.append(write_unicode(result.attributes[key].value))
+                    wt.writerow(addList)
+                else:
+                    omit += 1
+        print("Logged " + filename + ", Omitted " + str(omit) + " records.")
+
+filelist = [ f for f in os.listdir(".") if f.endswith(".csv") ]
+for f in filelist:
+    removeDuplicates(f)
