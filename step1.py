dba5cc5577e3080da9ab3dd87d71bb245870486a
Bigbrass <donrwalsh@gmail.com>
2017-06-19 09:58:48 -0500

    mailController is ready to go (except errors)
    Condensed functionality into 4 methods to be imported and used by a
    main function.

--- step1.py
+++ step1.py
@@ -1,71 +1,22 @@
 from __future__ import print_function
 import httplib2
 import os
-import pprint
-import email
-import base64
-import requests
 from datetime import datetime
 import csv
 from apiclient import discovery
-from oauth2client import client
 from oauth2client import tools
-from oauth2client.file import Storage
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+
+import mailController
+
+
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'wifi-anayzer'
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    credential_path = os.path.join(os.getcwd(), 'oauth.json')
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-def getMessageBody(service, user_id, msg_id):
-    try:
-            message = service.users().messages().get(userId=user_id, id=msg_id, format='raw').execute()
-            msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
-            mime_msg = email.message_from_string(msg_str)
-            messageMainType = mime_msg.get_content_maintype()
-            if messageMainType == 'multipart':
-                    for part in mime_msg.get_payload():
-                            if part.get_content_maintype() == 'text':
-                                    return part.get_payload()
-                    return ""
-            elif messageMainType == 'text':
-                    return mime_msg.get_payload()
-    except requests.HttpError, error:
-            print('An error occurred: %s' % error)
 
 def main():
     #Create a Gmail API service object for queries:
-    credentials = get_credentials()
+    credentials = mailController.getCredentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
@@ -91,7 +42,7 @@ def main():
         print(str(i) + "/" + str(count) + " " + str((i*100)/count) + "% Complete. " + "Creating " + name)
         #Create the file:
         file = open(folderName + "/" + name, "w")
-        file.write(getMessageBody(service, 'me', msg['id'])[:-60])
+        file.write(mailController.getMessageBody(service, 'me', msg['id'])[:-60])
         file.close()
         #If it does not exist, create the manifest. Otherwise, add an entry
         if os.path.isfile(manifest):
